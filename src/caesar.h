8e866e28bdd499f3829acc87771407cac2a82a38
lnikon <nreplay@list.ru>
2017-10-13 22:27:08 +0400

    fixed untracked files

---
+++ src/caesar.h
@@ -0,0 +1,3 @@
+void chiper(char *pPlainText, const int &rSize, char *pChiperText, const int &rKey);
+void dechiper(char *pChiperText, const int &rSize, char *pHypothetiquePlainText, const int &rKey);
+int check(char *pPlainText, char *pHypothetiquePlainText);
---
+++ src/util.h
@@ -0,0 +1,2 @@
+void printArray(const char *pA, const int &rSize);
+void writeToArray(char *pA, const int &rSize);
